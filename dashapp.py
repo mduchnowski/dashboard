a340f3e04deeaefe2f28094714959d28ce6d8f99
mduchnowski <mduchnowski@ets.org>
2023-06-23 18:22:01 -0400

    no comment

--- dashapp.py
+++ dashapp.py
@@ -12,6 +12,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
 import plotly.figure_factory as ff
+import plotly.subplots as sp
 
 import streamlit as st
 from streamlit_option_menu import option_menu 
@@ -48,13 +49,25 @@ def days_until_payday(date):
 
     return days_until_payday
 
+# Function for finding how many days until payday 
+@st.cache(suppress_st_warning=True)
+def read_data_from_excel(workbook_name, sheet_name):
+    try:
+        df = pd.read_excel(".".join([workbook_name, "xlsx"]), sheet_name=sheet_name)
+        return df
+    except FileNotFoundError:
+        print(f"File '{workbook_name}' not found.")
+        return None
+    except Exception as e:
+        print("An error occurred while reading the Excel file.")
+        print(e)
+        return None
 
+dfAllotment = read_data_from_excel("ledger","allotment")
+dfEntries = read_data_from_excel("ledger", sheet_name="entries")
 
-
-
-
-
-
+# Left join on 'Category' column and replace NaN with 0
+df = dfAllotment.merge(dfEntries.groupby('Category')['Spent'].sum().reset_index(), on='Category', how='left').fillna(0)
 
 # Set the desired sidebar width
 sidebar_width = 100
@@ -87,9 +100,22 @@ tbody th {
 </style>
 """
 
+# CSS styling
+centered_style = '''
+    <style>
+    .centered-text {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        text-align: center;
+    }
+    </style>
+    '''
+
+
 # Apply the custom CSS styles using a single st.markdown call
 st.markdown(custom_styles, unsafe_allow_html=True)
-
 
 st.sidebar.markdown('<p class="header-text">Dashboard</p>', unsafe_allow_html=True)
 
@@ -106,6 +132,70 @@ with st.sidebar:
             icons = ["activity"],
             default_index=0
         )
-        
 if selected == "Dashboard":
-    st.write(str(days_until_payday(datetime.today().date())))
+    #st.write(str(days_until_payday(datetime.today().date())))
+    #st.dataframe(dfEntries)    
+    #st.dataframe(dfAllotment)
+    #st.dataframe(df)
+    
+    # Sample DataFrame
+    #data = {
+    #    'Category': ['Category A', 'Category B', 'Category C'],
+    #    'Budget': [1000, 2000, 1500],
+    #    'Spent': [600, 1200, 1000]
+    #}
+    #df = pd.DataFrame(data)
+
+    # Calculate the percentage spent for each category
+    df['Percentage Spent'] = df['Spent'] / df['Budget'] * 100
+
+    # Create a single column using st.columns
+    col0 = st.columns([100])
+
+    
+    
+    # Display pie chart and bar chart for each category
+    for _, row in df.iterrows():
+        category = row['Category']
+        budget = row['Budget'] 
+        spent = row['Spent']
+        percentage_spent = row['Percentage Spent']
+
+        # Create a centered container for the title
+        title_container = col0[0].container()
+        with title_container:
+            st.markdown('<div style="text-align:center"><h2>{}</h2></div>'.format(category), unsafe_allow_html=True)
+
+        # Pie chart
+        fig_pie = go.Figure(go.Pie(
+            labels=['Spent', 'Remaining'],
+            values=[spent, budget - spent],
+            marker_colors=['blue', 'lightblue'],
+            textinfo='label+percent',
+            title=f'{category}<br>{percentage_spent:.2f}% Spent'
+        ))
+        fig_pie.update_layout(showlegend=False)
+
+        # Bar chart
+        fig_bar = go.Figure(go.Bar(
+            x=['Spent'],
+            y=[spent],
+            marker_color=['blue', 'lightblue'],
+            text=[spent, budget - spent],
+            textposition='auto',
+            hoverinfo='none'
+        ))
+        fig_bar.update_layout(
+            title=f'{category} - Budget vs. Spent',
+            yaxis=dict(range=[0, budget]),
+            showlegend=False
+        )
+
+        # Display the title and the graphs
+        with col0[0]:
+            st.plotly_chart(fig_pie, use_container_width=True)
+            st.plotly_chart(fig_bar, use_container_width=True)
+            
+            
+            
+            
---
+++ dashboard.bat
@@ -0,0 +1,4 @@
+call C:\Users\mduchnowski\Anaconda3\Scripts\activate.bat C:\Users\mduchnowski\Anaconda3
+cd C:\Users\mduchnowski\OneDrive - Educational Testing Service\Documents\Personal\Budget\Dashboard\dashboard
+streamlit run dashapp.py
+pause
